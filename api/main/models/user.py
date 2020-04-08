8529f1f4932c190a59d2939e8620bf529da5db5a
YoelRoger <yoelfernandez2008@hotmail.com>
2020-04-07 21:24:04 -0300

    seism-usr-sensor

--- api/main/models/user.py
+++ api/main/models/user.py
@@ -4,7 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    admin = db.Column(db.Boolean)
+    admin = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
-        return '<User: %r %r >' % (self.email, self.password, self.admin)
+        return '<User: %r %r %r>' % (self.email, self.password, self.admin)
